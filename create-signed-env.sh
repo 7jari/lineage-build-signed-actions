112997c67a9e6dce4f28609b902f79698cca7d56
Pranav Vashi <neobuddy89@gmail.com>
2026-01-26 18:15:13 +0530

    Fix key generation and extend life of enter key
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -51,12 +51,20 @@ if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
 fi
 clear
 
+make_key_nopass() {
+  local out="$1"
+  local size="${2:-2048}"
+
+  bash <(
+    sed "s/2048/${size}/;/Enter password/,+1d" ./development/tools/make_key
+  ) "$out" "$subject"
+}
+
 # Create Key
-echo "Press ENTER TWICE to skip password (around 100 enter hits total). Cannot use a password for inline signing!"
 mkdir -p ~/.android-certs
 
 for cert in bluetooth cts_uicc_2021 cyngn-app media networkstack nfc platform releasekey sdk_sandbox shared testcert testkey verity; do \
-    ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
+  make_key_nopass ~/.android-certs/$cert 2048
 done
 
 # Create APEX keys
@@ -64,8 +72,13 @@ cp ./development/tools/make_key ~/.android-certs/
 sed -i 's|2048|4096|g' ~/.android-certs/make_key
 
 for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.appsearch.apk com.android.art com.android.bluetooth com.android.bt com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.crashrecovery com.android.devicelock com.android.extservices com.android.federatedcompute com.android.graphics.pdf com.android.hardware.authsecret com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.contexthub com.android.hardware.dumpstate com.android.hardware.gatekeeper.nonsecure com.android.hardware.neuralnetworks com.android.hardware.power com.android.hardware.rebootescrow com.android.hardware.thermal com.android.hardware.threadnetwork com.android.hardware.uwb com.android.hardware.vibrator com.android.hardware.wifi com.android.health.connect.backuprestore com.android.healthconnect.controller com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.nfcservices com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.profiling com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonycore com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uprobestats com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb; do \
-    ~/.android-certs/make_key ~/.android-certs/$apex.certificate.override "$subject"; \
-    openssl pkcs8 -in ~/.android-certs/$apex.certificate.override.pk8 -inform DER -nocrypt -out ~/.android-certs/$apex.certificate.override.x509.pem; \
+    make_key_nopass ~/.android-certs/$apex.certificate.override 4096
+
+    if ! grep -q "BEGIN CERTIFICATE" ~/.android-certs/$apex.certificate.override.x509.pem; then
+        echo "ERROR: Invalid cert PEM for $apex"
+        head -n 5 ~/.android-certs/$apex.certificate.override.x509.pem || true
+        exit 1
+    fi
 done
 
 ## Create vendor for keys
