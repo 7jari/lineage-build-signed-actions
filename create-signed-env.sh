3b44a9586b798bc7c62635da4e08039ecb0f1041
7jari <zadigsm@outlook.com>
2026-04-06 21:26:32 +0700

    Add GitHub Actions workflow and adapt script for non-interactive sessions

--- create-signed-env.sh
+++ create-signed-env.sh
@@ -1,55 +1,11 @@
 #!/bin/bash
 
-# Default values
-DEFAULT_COUNTRY="US"
-DEFAULT_STATE="California"
-DEFAULT_LOCALITY="Mountain View"
-DEFAULT_ORGANIZATION="crDroid"
-DEFAULT_ORG_UNIT="crDroid"
-DEFAULT_COMMON_NAME="crDroid"
-DEFAULT_EMAIL="contact@crdroid.net"
-
-# Prompt the user for each part of the subject line with defaults
-read -p "Enter country code [${DEFAULT_COUNTRY}] (C): " country
-country=${country:-$DEFAULT_COUNTRY}
-
-read -p "Enter state or province name [${DEFAULT_STATE}] (ST): " state
-state=${state:-$DEFAULT_STATE}
-
-read -p "Enter locality [${DEFAULT_LOCALITY}] (L): " locality
-locality=${locality:-$DEFAULT_LOCALITY}
-
-read -p "Enter organization name [${DEFAULT_ORGANIZATION}] (O): " organization
-organization=${organization:-$DEFAULT_ORGANIZATION}
-
-read -p "Enter organizational unit [${DEFAULT_ORG_UNIT}] (OU): " organizational_unit
-organizational_unit=${organizational_unit:-$DEFAULT_ORG_UNIT}
-
-read -p "Enter common name [${DEFAULT_COMMON_NAME}] (CN): " common_name
-common_name=${common_name:-$DEFAULT_COMMON_NAME}
-
-read -p "Enter email address [${DEFAULT_EMAIL}] (emailAddress): " email
-email=${email:-$DEFAULT_EMAIL}
-
 # Construct the subject line
 subject="/C=${country}/ST=${state}/L=${locality}/O=${organization}/OU=${organizational_unit}/CN=${common_name}/emailAddress=${email}"
 
 # Print the subject line
-echo ""
 echo "Using Subject Line:"
 echo "$subject"
-echo ""
-
-# Prompt the user to verify if the subject line is correct
-read -p "Is the subject line correct? [Y/n]: " confirmation
-confirmation=${confirmation:-Y}
-
-# Check the user's response
-if [[ $confirmation != "y" && $confirmation != "Y" ]]; then
-    echo "Exiting without changes."
-    exit 1
-fi
-clear
 
 make_key_nopass() {
   local out="$1"
@@ -131,4 +87,3 @@ echo "✓ Done! Now build as usual."
 echo "✓ If builds aren't being signed, add '-include vendor/lineage-priv/keys/keys.mk' to your device mk file"
 echo ""
 echo "⚠ IMPORTANT: Make copies of your vendor/lineage-priv folder as it contains your keys!"
-sleep 3
